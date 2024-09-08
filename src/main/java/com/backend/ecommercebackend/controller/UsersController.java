419c255ad6d1cc5f30d646c1e6d7c1f4ac689c29
Nicat-Dev <nijatmehdiyevv@gmail.com>
2024-09-08 19:34:27 +0200

    refactored

--- src/main/java/com/backend/ecommercebackend/controller/UsersController.java
+++ src/main/java/com/backend/ecommercebackend/controller/UsersController.java
@@ -1,7 +1,9 @@
 package com.backend.ecommercebackend.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class UsersController {
 }
