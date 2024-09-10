4b6dffc66e36995e0a29214ac4dda2afec9e075c
FaridGanbarov <faridqanbarov3@gmail.com>
2024-09-10 15:56:28 +0400

    endRefreshToken

--- src/main/java/com/backend/ecommercebackend/service/TokenService.java
+++ src/main/java/com/backend/ecommercebackend/service/TokenService.java
@@ -1,9 +1,9 @@
-package com.backend.ecommercebackend.service;
+package com.backend.ecommercebackend.authentication.service;
 
 import io.jsonwebtoken.Claims;
 
 public interface TokenService {
-    String generateAccessToken(String username);
+
     String generateRefreshToken(String username);
     Claims validateToken(String token);
 }
--- src/main/java/com/backend/ecommercebackend/service/impl/TokenServiceImpl.java
+++ src/main/java/com/backend/ecommercebackend/service/impl/TokenServiceImpl.java
@@ -1,6 +1,5 @@
-package com.backend.ecommercebackend.service.impl;
+package com.backend.ecommercebackend.authentication.service;
 
-import com.backend.ecommercebackend.service.TokenService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -10,19 +9,8 @@ import java.util.Date;
 
 @Service
 public class TokenServiceImpl implements TokenService {
-    private static final long ACCESS_TOKEN_VALIDITY = 15 * 60 * 1000;
     private static final long REFRESH_TOKEN_VALIDITY = 30 * 24 * 60 * 60 * 1000;
     private static final String SECRET_KEY = "Gz7#fD3!qR9@un2*Hj4$wLk8&vXz5#Yp";
-
-    @Override
-    public String generateAccessToken(String username) {
-        return Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis()+ ACCESS_TOKEN_VALIDITY))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
-                .compact();
-    }
 
     @Override
     public String generateRefreshToken(String username) {
