e869bf11c4fd0240d7caac360f26ba629d007702
YeganaKhalilova <y.khalilova2@newcastle.ac.uk>
2024-09-12 11:59:19 +0400

    reAdded logout impl

--- src/main/java/com/backend/ecommercebackend/authentication/service/serviceImpl/AuthenticationServiceImpl.java
+++ src/main/java/com/backend/ecommercebackend/authentication/service/serviceImpl/AuthenticationServiceImpl.java
@@ -1,11 +1,13 @@
 package com.backend.ecommercebackend.authentication.service.serviceImpl;
 
+import com.backend.ecommercebackend.authentication.dto.request.LogoutRequest;
 import com.backend.ecommercebackend.authentication.jwt.JwtService;
 import com.backend.ecommercebackend.authentication.dto.request.AuthRequest;
 import com.backend.ecommercebackend.authentication.mapper.AuthMapper;
 import com.backend.ecommercebackend.authentication.model.AuthResponse;
 import com.backend.ecommercebackend.authentication.dto.request.RegisterRequest;
 import com.backend.ecommercebackend.authentication.service.AuthenticationService;
+import com.backend.ecommercebackend.cache.service.RedisTokenService;
 import com.backend.ecommercebackend.model.Role;
 import com.backend.ecommercebackend.model.User;
 import com.backend.ecommercebackend.repository.UserRepository;
@@ -27,12 +29,15 @@ import java.time.LocalDateTime;
 @Service
 @RequiredArgsConstructor
 public class AuthenticationServiceImpl implements AuthenticationService {
+
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
     private final AuthMapper authMapper;
     private final UserDetailsService userDetailsService;
+    private final RedisTokenService redisTokenService;
+
     @Override
     public AuthResponse register(RegisterRequest request) {
         User user = authMapper.RegisterDtoToEntity(request,passwordEncoder);
@@ -66,6 +71,16 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     }
 
     @Override
+    public void logout(LogoutRequest request) {
+        var token = request.getRefreshToken();
+        String email = request.getEmail();
+        if (redisTokenService.validateRefreshToken(email, token)) {
+            redisTokenService.deleteRefreshToken(email);
+        } else {
+            throw new IllegalArgumentException("Invalid refresh token");
+        }
+    }
+    @Override
     public void refreshAuthToken(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String userEmail;
         String refreshToken;
@@ -87,5 +102,4 @@ public class AuthenticationServiceImpl implements AuthenticationService {
             }
         }
     }
-
 }
