5e49925374a25c4fb2f7caca8b2009cac1293183
YeganaKhalilova <y.khalilova2@newcastle.ac.uk>
2024-09-12 17:15:34 +0400

    fixed mapping

--- src/main/java/com/backend/ecommercebackend/cache/controller/RedisTokenController.java
+++ src/main/java/com/backend/ecommercebackend/cache/controller/RedisTokenController.java
@@ -1,9 +1,9 @@
 package com.backend.ecommercebackend.cache.controller;
 
-
 import com.backend.ecommercebackend.cache.service.RedisTokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,7 +16,7 @@ public class RedisTokenController {
 
   private  final RedisTokenService redisTokenService;
 
-  @PostMapping("/get")
+  @GetMapping("/get")
   public ResponseEntity<String> getRefreshToken(@RequestParam("email") String email) {
     String refreshToken = redisTokenService.getRefreshToken(email);
     return ResponseEntity.ok(refreshToken);
