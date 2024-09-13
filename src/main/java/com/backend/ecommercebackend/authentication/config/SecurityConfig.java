f7bc832c27986f26fbf66ad3703a5976ae8e46b8
YeganaKhalilova <y.khalilova2@newcastle.ac.uk>
2024-09-13 14:49:05 +0400

    modified allowed ports in security

--- src/main/java/com/backend/ecommercebackend/authentication/config/SecurityConfig.java
+++ src/main/java/com/backend/ecommercebackend/authentication/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8081", "http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:8081", "http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
