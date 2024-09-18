20572c3c306e1a25e081d22e06fb3942d86beba9
YeganaKhalilova <y.khalilova2@newcastle.ac.uk>
2024-09-18 16:22:07 +0400

    added getter annotation

--- src/main/java/com/backend/ecommercebackend/exception/InvalidTokenException.java
+++ src/main/java/com/backend/ecommercebackend/exception/InvalidTokenException.java
@@ -1,17 +1,15 @@
 package com.backend.ecommercebackend.exception;
 
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 public class InvalidTokenException extends RuntimeException{
   private final HttpStatus httpStatus;
 
   public InvalidTokenException(HttpStatus httpStatus,String message) {
     super(message);
     this.httpStatus = httpStatus;
-  }
-
-  public HttpStatus getHttpStatus() {
-    return httpStatus;
   }
 }
