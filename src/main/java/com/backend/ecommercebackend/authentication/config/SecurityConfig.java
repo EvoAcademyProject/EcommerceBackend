05a24ddba496e8ca01501c221a96023fafe219e2
YeganaKhalilova <y.khalilova2@newcastle.ac.uk>
2024-09-27 19:50:45 +0400

    added delete user method

--- src/main/java/com/backend/ecommercebackend/authentication/config/SecurityConfig.java
+++ src/main/java/com/backend/ecommercebackend/authentication/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
                  )
                    .authorizeHttpRequests( auth ->
                     auth
-                            .requestMatchers("/api/v1/auth/**","/api/v1/product/**").permitAll()
+                            .requestMatchers("/api/v1/auth/**","/api/v1/product/**","/api/v1/user/**").permitAll()
                             .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                             .anyRequest().authenticated()
                 )
