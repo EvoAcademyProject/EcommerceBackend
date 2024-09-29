9d0983f1c484e1c382abfb39bb473cc988adba33
FaridGanbarov <faridqanbarov3@gmail.com>
2024-09-29 22:57:30 +0400

    ImageFile controller Part

--- src/main/java/com/backend/ecommercebackend/service/impl/ProductServiceImpl.java
+++ src/main/java/com/backend/ecommercebackend/service/impl/ProductServiceImpl.java
@@ -9,9 +9,7 @@ import com.backend.ecommercebackend.model.product.Product;
 import com.backend.ecommercebackend.repository.product.ProductRepository;
 import com.backend.ecommercebackend.service.FileStorageService;
 import com.backend.ecommercebackend.service.ProductService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -28,24 +26,14 @@ public class ProductServiceImpl implements ProductService {
     private final FileStorageService fileStorageService;
 
     @Override
-    public ProductResponse addProduct(ProductRequest request, MultipartFile imageFile) {
-        if (request.getSpecifications() != null) {
-            try {
-                ObjectMapper objectMapper = new ObjectMapper();
-                List<Object> specifications = objectMapper.readValue(request.getSpecifications().toString(), new TypeReference<>() {});
-                request.setSpecifications(specifications);
-            } catch (JsonProcessingException e) {
-                throw new ApplicationException(Exceptions.INVALID_FORMAT_EXCEPTION);
-            }
-        }
+    public ProductResponse addProduct(ProductRequest request,MultipartFile imageFile) {
         Product product = mapper.ProductDtoToEntity(request);
         if (imageFile != null && !imageFile.isEmpty()) {
             try {
-                String fileName = fileStorageService.storeFile(imageFile);
-                String imageUrl = "https://ff82f4df-f72b-4dec-84ca-487132aff620.mock.pstmn.io/uploads/" + fileName;
+                String imageUrl = fileStorageService.storeFile(imageFile);
                 product.setImageUrl(imageUrl);
             } catch (IOException e) {
-                throw new ApplicationException(Exceptions.IMAGE_STORAGE_EXCEPTION);
+                throw new RuntimeException("Failed to store image", e);
             }
         }
         repository.save(product);
@@ -65,8 +53,11 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public ProductResponse updateProduct(Long id, ProductRequest request, MultipartFile imageFile) {
-        Product product = repository.findById(id).orElseThrow(() -> new ApplicationException(Exceptions.NOT_FOUND_EXCEPTION));
+        Product product = repository.findById(id).orElseThrow(() -> new RuntimeException("Product not found with id: " + id));
+
+
         mapper.updateProductFromProductDto(request, product);
+
         if (imageFile != null && !imageFile.isEmpty()) {
             try {
                 String imageUrl = fileStorageService.storeFile(imageFile);
@@ -76,7 +67,8 @@ public class ProductServiceImpl implements ProductService {
                 }
                 product.setImageUrl(imageUrl);
             } catch (IOException e) {
-                throw new ApplicationException(Exceptions.IMAGE_STORAGE_EXCEPTION);            }
+                throw new RuntimeException("Failed to store image", e);
+            }
         }
         repository.save(product);
         return mapper.EntityToProductDto(product);
@@ -84,14 +76,17 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public void deleteProduct(Long id) {
-        Product product = repository.findById(id).orElseThrow(() -> new ApplicationException(Exceptions.NOT_FOUND_EXCEPTION));
-        if(product.getImageUrl()!=null){
-            try{
+        Product product = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found with id: " + id));
+
+        if (product.getImageUrl() != null) {
+            try {
                 fileStorageService.deleteFile(product.getImageUrl());
             } catch (IOException e) {
-                log.error("Failed to delete image");
+                throw new RuntimeException("Failed to delete image", e);
             }
         }
+
         repository.deleteById(id);
     }
 
